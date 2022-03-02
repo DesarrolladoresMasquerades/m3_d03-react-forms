953aab0371c0cb38df4886ed667f9616c92ce4ed
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-02 16:00:49 +0100

    done class

--- src/components/FilterMovies.js
+++ src/components/FilterMovies.js
@@ -1,8 +1,14 @@
 import { useState } from "react";
 
 function FilterMovies(props) {
+  const [firstLetter, setFirstLetter] = useState("All");
 
-  
+  const handleSelect = (e) => {
+    setFirstLetter(e.target.value);
+
+    // console.log("selected", e.target.value);
+    props.filterMovies(e.target.value);
+  };
 
   return (
     <div className="FilterMovies">
